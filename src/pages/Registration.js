8bf2298f37dc36d8b0b27016fdcb13cbd4b6100b
jihun8861 <ji55hun@oasis.inje.ac.kr>
2024-07-29 00:44:30 +0900

    feat: Registration

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Main from "../components/Main";
+import { RiRadioButtonFill } from "react-icons/ri";
+import { GoPlus } from "react-icons/go";
+import DaumPostcodeEmbed from "react-daum-postcode";
 
 const Container = styled.div`
   width: 100%;
-  height: 1500px;
+  height: auto;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
 
 const Frame = styled.div`
@@ -19,11 +23,12 @@ const TitleText = styled.h1`
   display: flex;
   justify-content: center;
   font-size: 46px;
-  margin: 50px 0 50px 0;
+  margin-top: 50px;
 `;
 const InfoFrame = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-top: 70px;
 `;
 
 const InfoTitle = styled.h2`
@@ -35,7 +40,6 @@ const TopLine = styled.hr`
   margin-top: 20px;
 `;
 
-// 박스의 전체적인 크기 프레임
 const BoxFrame = styled.div`
   width: 100%;
   height: auto;
@@ -46,7 +50,7 @@ const BoxFrame = styled.div`
 `;
 
 const BoxName = styled.div`
-  width: 10%;
+  width: 12%;
   height: auto;
   display: flex;
   align-items: center;
@@ -58,9 +62,8 @@ const BoxName = styled.div`
   border-right: solid 1px #e0e0e0;
 `;
 
-// 박스의 오른쪽 정보 프레임
 const BoxInfo = styled.div`
-  width: 90%;
+  width: 88%;
   height: 100%;
   padding: 25px;
   display: flex;
@@ -97,10 +100,6 @@ const TextBox = styled.input`
 
   &:hover {
     border: solid 0.5px #dbdbdb;
-  }
-
-  &:focus {
-    border: solid 1px #222222;
   }
 `;
 
@@ -142,16 +141,225 @@ const BoxInfoMap = styled.div`
   border-radius: 3px;
 `;
 
+const DropdownContainer = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Dropdown = styled.select`
+  padding-left: 20px;
+  font-size: 18px;
+  width: 150px;
+  height: 60px;
+  border: solid 1px #dbdbdb;
+  transition: border 0.5s ease;
+  background-color: ${(props) => (props.disabled ? "#f5f5f5" : "white")};
+  color: ${(props) => (props.disabled ? "#a0a0a0" : "black")};
+  pointer-events: ${(props) => (props.disabled ? "none" : "auto")};
+`;
+
+const Option = styled.option`
+  font-size: 18px;
+`;
+
+const RadioGroup = styled.div`
+  display: flex;
+`;
+
+const RadioLabel = styled.label`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+  font-size: 19px;
+  cursor: pointer;
+  svg {
+    margin-right: 5px;
+  }
+`;
+
+const ImageBtn = styled.button`
+  position: relative;
+  padding-left: 20px;
+  font-size: 18px;
+  width: 170px;
+  height: 50px;
+  border: solid 1px #cecece;
+  transition: border 0.5s ease;
+  cursor: pointer;
+  background-color: transparent;
+  font-weight: bold;
+
+  &:hover {
+    border: solid 0.5px #dbdbdb;
+    background-color: #f8f8f8;
+    transition: background-color 0.3s ease;
+  }
+`;
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  font-size: 18px;
+  border: solid 1px #dbdbdb;
+  transition: border 0.5s ease;
+  line-height: 1.6;
+  resize: none;
+
+  &:hover {
+    border: solid 0.5px #dbdbdb;
+    transition: background-color 0.3s ease;
+  }
+
+  &::placeholder {
+    white-space: pre-wrap;
+    text-indent: 0;
+  }
+`;
+
+const CharCount = styled.div`
+  font-size: 15px;
+  color: #888;
+  margin: 5px 0 0 5px;
+  text-align: left;
+`;
+
+const RegistrationBtnSpace = styled.div`
+  display: flex;
+  algin-items: center;
+  justify-content: center;
+  padding: 100px 0 100px 0;
+`
+
+const RegistrationBtn = styled.button`
+  width: 220px;
+  height: 80px;
+  color:white;
+  font-size: 19px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: #FE8C12;
+  border: none;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #f08e24;
+  }
+`
+
+const PlusIcon = styled(GoPlus)`
+  font-size: 24px;
+  position: absolute;
+  left: 30px;
+  top: 12px;
+`
+
+const Modal = styled.div`
+  width: 500px;
+  height: 500px;
+  border: solid 1px;
+  z-index: 100;
+position: absolute;
+top: 50px;
+left: 250px;
+`
+
 const RegistrationContent = () => {
+  const [selectedFloor, setSelectedFloor] = useState("");
+  const [selectedSecondFloor, setSelectedSecondFloor] = useState("");
+  const [elevator, setElevator] = useState("없음");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [areaPyeong, setAreaPyeong] = useState("");
+  const [areaSquareMeter, setAreaSquareMeter] = useState("");
+  const [roomCount, setRoomCount] = useState("");
+  const [postCode, setPostCode] = useState("");
+  const [modal, setModal] = useState(false);
+
+  const handleFirstDropdownChange = (e) => {
+    setSelectedFloor(e.target.value);
+    setSelectedSecondFloor("");
+  };
+
+  const renderOptions = (count) => {
+    return Array.from({ length: count }, (_, index) => (
+      <Option key={index + 1} value={index + 1}>
+        {index + 1}층
+      </Option>
+    ));
+  };
+
+  const handleTitleChange = (e) => {
+    const { value } = e.target;
+    if (value.length <= 40) {
+      setTitle(value);
+    }
+  };
+
+  const handleDescriptionChange = (e) => {
+    const { value } = e.target;
+    if (value.length <= 1000) {
+      setDescription(value);
+    }
+  };
+
+  const handleAreaPyeongChange = (e) => {
+    const { value } = e.target;
+    if (value === "" || /^[0-9\b]+$/.test(value)) {
+      setAreaPyeong(value);
+      if (value) {
+        setAreaSquareMeter((value * 3.3058).toFixed(2));
+      } else {
+        setAreaSquareMeter("");
+      }
+    }
+  };
+
+  const handleAreaSquareMeterChange = (e) => {
+    const { value } = e.target;
+    if (value === "" || /^[0-9\b]+$/.test(value)) {
+      setAreaSquareMeter(value);
+      if (value) {
+        setAreaPyeong((value / 3.3058).toFixed(2));
+      } else {
+        setAreaPyeong("");
+      }
+    }
+  };
+
+  const handleRoomCountChange = (e) => {
+    const { value } = e.target;
+    if (value === "" || /^[0-9\b]+$/.test(value)) {
+      setRoomCount(value);
+    }
+  };
+
+  const handleSelectComplete = (data) => {
+    const adress = data.adress;
+    setPostCode(adress);
+    setModal(false);
+    console.log(adress);
+  }
+
   return (
     <Container>
+    
+      {modal && (
+        <Modal>
+        <DaumPostcodeEmbed style={{ width: "100%", height: "100%" }}
+          onComplete={handleSelectComplete}/>
+        </Modal>
+      )}
+      
       <Frame>
         <TitleText>방 등록</TitleText>
         <InfoFrame>
           <InfoTitle>매물 정보</InfoTitle>
-          <h3>* 필수입력 항목</h3>
+          <h3><span style={{ color: "#FE8C12",marginRight: "4px"}}>*</span>필수입력 항목</h3>
         </InfoFrame>
         <TopLine />
+
         <BoxFrame>
           <BoxName>
             <h4>매물 주소</h4>
@@ -164,7 +372,7 @@ const RegistrationContent = () => {
                 <TextBoxContainer>
                   <TextBox placeholder="예) 번동 10-1, 강북구 번동" />
                 </TextBoxContainer>
-                <SearchBtn>검색</SearchBtn>
+                <SearchBtn onClick={()=>{setModal(true)}}>검색</SearchBtn>
               </TextBoxFrame>
             </SearchFrame>
             <BoxInfoMap>
@@ -183,14 +391,24 @@ const RegistrationContent = () => {
               <h4>전용 면적</h4>
               <TextBoxFrame>
                 <TextBoxContainer>
-                  <TextBox style={{ width: "150px" }} placeholder="평수 입력" />
+                  <TextBox
+                    style={{ width: "150px" }}
+                    placeholder="평수 입력"
+                    value={areaPyeong}
+                    onChange={handleAreaPyeongChange}
+                  />
                   <Unit>평</Unit>
                 </TextBoxContainer>
                 <span style={{ fontWeight: "bold", padding: "10px 10px" }}>
                   =
                 </span>
                 <TextBoxContainer>
-                  <TextBox style={{ width: "150px" }} placeholder="m² 입력" />
+                  <TextBox
+                    style={{ width: "150px" }}
+                    placeholder="m² 입력"
+                    value={areaSquareMeter}
+                    onChange={handleAreaSquareMeterChange}
+                  />
                   <Unit>m²</Unit>
                 </TextBoxContainer>
               </TextBoxFrame>
@@ -208,13 +426,205 @@ const RegistrationContent = () => {
               <h4>방 수</h4>
               <TextBoxFrame>
                 <TextBoxContainer>
-                  <TextBox style={{ width: "200px" }} />
+                  <TextBox
+                    style={{ width: "160px" }}
+                    value={roomCount}
+                    onChange={handleRoomCountChange}
+                  />
                   <Unit>개</Unit>
                 </TextBoxContainer>
               </TextBoxFrame>
             </SearchFrame>
           </BoxInfo>
         </BoxFrame>
+
+        <InfoFrame>
+          <InfoTitle>추가 정보</InfoTitle>
+        </InfoFrame>
+        <TopLine />
+
+        <BoxFrame>
+          <BoxName>
+            <h4>층 수</h4>
+            <span style={{ color: "#FE8C12", marginLeft: "4px" }}>*</span>
+          </BoxName>
+          <BoxInfo>
+            <SearchFrame>
+              <h4>전체층 수</h4>
+              <TextBoxFrame>
+                <DropdownContainer>
+                  <Dropdown
+                    value={selectedFloor}
+                    onChange={handleFirstDropdownChange}
+                  >
+                    <Option value="">선택</Option>
+                    {renderOptions(25)}
+                  </Dropdown>
+                </DropdownContainer>
+                <DropdownContainer style={{ marginLeft: "40px" }}>
+                  <Dropdown
+                    value={selectedSecondFloor}
+                    onChange={(e) => setSelectedSecondFloor(e.target.value)}
+                    disabled={!selectedFloor}
+                  >
+                    <Option value="">선택</Option>
+                    {selectedFloor &&
+                      renderOptions(parseInt(selectedFloor, 10))}
+                  </Dropdown>
+                </DropdownContainer>
+              </TextBoxFrame>
+            </SearchFrame>
+          </BoxInfo>
+        </BoxFrame>
+
+        <BoxFrame>
+          <BoxName>
+            <h4>엘리베이터</h4>
+            <span style={{ color: "#FE8C12", marginLeft: "4px" }}>*</span>
+          </BoxName>
+          <BoxInfo>
+            <SearchFrame>
+              <TextBoxFrame>
+                <RadioGroup>
+                  <RadioLabel onClick={() => setElevator("없음")}>
+                    <RiRadioButtonFill
+                      color={elevator === "없음" ? "#FE8C12" : "#ccc"}
+                      size={30}
+                    />
+                    없음
+                  </RadioLabel>
+                  <RadioLabel onClick={() => setElevator("있음")}>
+                    <RiRadioButtonFill
+                      color={elevator === "있음" ? "#FE8C12" : "#ccc"}
+                      size={30}
+                    />
+                    있음
+                  </RadioLabel>
+                </RadioGroup>
+              </TextBoxFrame>
+            </SearchFrame>
+          </BoxInfo>
+        </BoxFrame>
+
+        <InfoFrame>
+          <InfoTitle>시설 정보</InfoTitle>
+        </InfoFrame>
+        <TopLine />
+
+        <BoxFrame>
+          <BoxName>
+            <h4>난방 시설</h4>            
+          </BoxName>
+          <BoxInfo>
+
+          </BoxInfo>
+        </BoxFrame>
+
+        <BoxFrame>
+          <BoxName>
+            <h4>냉방 시설</h4>
+          </BoxName>
+          <BoxInfo>
+
+          </BoxInfo>
+        </BoxFrame>
+
+        <BoxFrame>
+          <BoxName>
+            <h4>생활 시설</h4>
+          </BoxName>
+          <BoxInfo>
+
+          </BoxInfo>
+        </BoxFrame>
+
+        <BoxFrame>
+          <BoxName>
+            <h4>보안 시설</h4>
+          </BoxName>
+          <BoxInfo>
+
+          </BoxInfo>
+        </BoxFrame>
+
+        <BoxFrame>
+          <BoxName>
+            <h4>기타 시설</h4>
+          </BoxName>
+          <BoxInfo>
+
+          </BoxInfo>
+        </BoxFrame>
+
+        <InfoFrame>
+          <InfoTitle>사진 등록</InfoTitle>
+        </InfoFrame>
+        <TopLine />
+
+        <BoxFrame>
+          <BoxName>
+            <h4>일반 사진</h4>
+            <span style={{ color: "#FE8C12", marginLeft: "4px" }}>*</span>
+          </BoxName>
+          <BoxInfo>
+            <SearchFrame>
+              <ImageBtn><PlusIcon/>사진추가</ImageBtn>
+            </SearchFrame>
+          </BoxInfo>
+        </BoxFrame>
+
+        <InfoFrame>
+          <InfoTitle>상세 설명</InfoTitle>
+        </InfoFrame>
+        <TopLine />
+
+        <BoxFrame>
+          <BoxName>
+            <h4>제목</h4>
+            <span style={{ color: "#FE8C12", marginLeft: "4px" }}>*</span>
+          </BoxName>
+          <BoxInfo>
+            <SearchFrame>
+              <TextBoxFrame>
+                <TextBoxContainer>
+                  <TextBox
+                    style={{ width: "100%" }}
+                    placeholder="리스트에 노출되는 문구입니다. 40자 이내로 작성해주세요."
+                    value={title}
+                    onChange={handleTitleChange}
+                  />
+                  <CharCount>{title.length}/40</CharCount>
+                </TextBoxContainer>
+              </TextBoxFrame>
+            </SearchFrame>
+          </BoxInfo>
+        </BoxFrame>
+
+        <BoxFrame>
+          <BoxName>
+            <h4>상세설명</h4>
+            <span style={{ color: "#FE8C12", marginLeft: "4px" }}>*</span>
+          </BoxName>
+          <BoxInfo>
+            <SearchFrame>
+              <TextBoxFrame>
+                <TextBoxContainer>
+                  <TextArea
+                    placeholder="매물 상세 페이지에 노출되는 문구입니다.
+                    1000자 이내로 작성해주세요."
+                    value={description}
+                    onChange={handleDescriptionChange}
+                  />
+                  <CharCount>{description.length}/1000</CharCount>
+                </TextBoxContainer>
+              </TextBoxFrame>
+            </SearchFrame>
+          </BoxInfo>
+        </BoxFrame>
+        
+        <RegistrationBtnSpace>
+        <RegistrationBtn>방 등록</RegistrationBtn>
+        </RegistrationBtnSpace>
       </Frame>
     </Container>
   );
